d5ab9f31370c0913e8a3fdfc0602a16401caac43
havyas <havyasagaltha@gmail.com>
2023-12-02 19:47:08 +0530

    changed size of padding right in class first

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-const socket = io.connect('http://localhost:3000/');
+const socket = io.connect('https://chat4-r38x.onrender.com/');
 let name;
 let textarea = document.querySelector('#textarea');
 let messageArea = document.querySelector('.message__area');
@@ -82,4 +82,4 @@ function scrollToBottom() {
 // updateUserNotification(message,'help')
 // scrollToBottom();
 // });
-
+  
